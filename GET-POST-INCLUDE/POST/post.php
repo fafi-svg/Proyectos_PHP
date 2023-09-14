ac8b5fc4d75e4d9b58df061334ce422d6b266eab
fafi-svg <135574549+fafi-svg@users.noreply.github.com>
2023-09-14 16:30:24 -0500

    formulario get_form

---
+++ GET-POST-INCLUDE/POST/post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <form action="POST/post.php">
+            <div class="input__name">
+                <label for="name">NOMBRE</label>
+                <input id="name" type="text" name="nombre">
+            </div>
+            <div class="input__email">
+                <label for="email">EMAIL</label>
+                <input id="email" type="email" name="email">
+            </div>
+            <div class="input__submit">
+                <input type="submit" value="submit">
+            </div>
+        </form> 
+</body>
+</html>
